623b21f34e10190a7c5778531aca191bab9d9b93
P S Lathashree <38350210+PSLathashree@users.noreply.github.com>
2018-08-22 20:51:56 +0530

    Tarining and chat centre

---
+++ chat.js
@@ -0,0 +1,31 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var aaa=document.querySelector("input");
+ 
+aaa.addEventListener("click",function(){
+    alert(aaa.textContent);
+});
+/*(function() {
+
+	$('#live-chat header').on('click', function() {
+
+		$('.chat').slideToggle(300, 'swing');
+		$('.chat-message-counter').fadeToggle(300, 'swing');
+
+	});
+
+	$('.chat-close').on('click', function(e) {
+
+		e.preventDefault();
+		$('#live-chat').fadeOut(300);
+
+	});
+
+}) ();*/
+
+
+ 
